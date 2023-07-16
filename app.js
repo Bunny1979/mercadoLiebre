39c4c1db14e5db1936f11f2a50480da23fd6c6a9
Bunny1979 <noe_esteves@hotmail.com>
2023-07-16 20:36:42 -0300

    Creando proyecto ML

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    "Servidor escuchando en puerto " +
+      process.env.PORT +
+      "| http://localhost:3000"
+  );
+});
